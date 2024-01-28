af01f11927ff03d91574786390c6e43487808d7e
magnusjwatson2786 <62503589+magnusjwatson2786@users.noreply.github.com>
2024-01-28 18:13:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import nltk
+nltk.download('punkt')
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
